5b1f54fd1ab675993ada4c9e5d5f067e6cb17f87
berardo marquez <bmarquezucros@gmail.com>
2025-03-01 07:30:58 -0500

    ejercicio I/O

---
+++ src/entradaScaner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class entradaScaner {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = input.nextLine();
+        System.out.print("Ingrese su edad: ");
+        byte edad = input.nextByte();
+
+        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años.");
+
+        input.close();
+    }
+    
+}
