c7c73ccbdcc49db6779299680dacf072073cedd5
berardo <bmarquezucros@gmail.com>
2025-03-01 19:07:31 -0600

    ejercicio I/O se le agrego la altura

--- src/entradaScaner.java
+++ src/entradaScaner.java
@@ -8,8 +8,10 @@ public class entradaScaner {
         String nombre = input.nextLine();
         System.out.print("Ingrese su edad: ");
         byte edad = input.nextByte();
+        System.out.print("Ingrese su altura: ");
+        float altura = input.nextFloat();
 
-        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años.");
+        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años " + altura + " metros esta es tu altura. ");
 
         input.close();
     }
